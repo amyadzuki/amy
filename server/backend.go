6cd71f7cc29033a8682603a845083df94f0e54cb
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-07-21 03:18:37 +0000

    Fix: bug from early refactoring

--- server/backend.go
+++ server/backend.go
@@ -3,7 +3,7 @@ package server
 type Backend interface {
 	Body(...interface{}) []byte
 	Error(int, string, ...interface{})
-	Http(int, interface{}) error
+	Http(string, interface{}) error
 	Https(string, string, string, interface{}) error
 	Path(...interface{}) string
 	Write(string, ...interface{})
