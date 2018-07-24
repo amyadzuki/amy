7255bf50b3c0283e6ffce22856ffcdc45072fd03
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-07-24 12:07:03 +0000

    Update styles.go

--- styles/styles.go
+++ styles/styles.go
@@ -9,6 +9,6 @@ var CloseButton gui.ButtonStyles
 
 func init() {
 	s := gui.StyleDefault()
-	Close = s.Button
-	Close.Over.BgColor = math32.Color4{1, 0, 0, 1}
+	CloseButton = s.Button
+	CloseButton.Over.BgColor = math32.Color4{1, 0, 0, 1}
 }
