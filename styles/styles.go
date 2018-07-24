5d98b6b1cdad056517288acb9e31e11758e21cc4
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-07-24 12:33:16 +0000

    Added ClosingButtton

--- styles/styles.go
+++ styles/styles.go
@@ -6,9 +6,15 @@ import (
 )
 
 var CloseButton gui.ButtonStyles
+var ClosingButton gui.ButtonStyles
 
 func init() {
 	s := gui.StyleDefault()
+
 	CloseButton = s.Button
-	CloseButton.Over.BgColor = math32.Color4{1, 0, 0, 1}
+	CloseButton.Over.BgColor = math32.Color4{0.75, 0, 0, 1}
+
+	ClosingButton = s.Button
+	ClosingButton.Normal.BgColor = CloseButton.Over.BgColor
+	ClosingButton.Over.BgColor = math32.Color4{1, 0, 0, 1}
 }
