038e29883756eec7150890304e7b1f19845b19f8
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-07-25 00:35:27 +0000

    Rename

--- widget/perf.go
+++ widget/perf.go
@@ -19,11 +19,8 @@ func NewPerformance(large int, label string) (w *Performance) {
 
 func (w *Performance) Init(large int, label string) {
 	w.Panel = gui.NewPanel(0, 0)
-//	w.Panel.SetLayout(gui.NewDockLayout())
 	w.Units = gui.NewLabel(label)
-//	w.Units.SetLayoutParams(&gui.DockLayoutParams{gui.DockRight})
 	w.Value = gui.NewLabel(strconv.Itoa(large))
-//	w.Value.SetLayoutParams(&gui.DockLayoutParams{gui.DockRight})
 	w.Panel.Add(w.Units)
 	w.Panel.Add(w.Value)
 	uw, uh := float64(w.Units.TotalWidth()), float64(w.Units.TotalHeight())
