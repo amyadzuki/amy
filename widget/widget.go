2e92e3f52c6212548f3d2da9d3a4aeb7ba0a0ed7
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-07-25 00:06:46 +0000

    Bugfixes

--- widget/widget.go
+++ widget/widget.go
@@ -1,11 +1,23 @@
 package widget
 
+import (
+	"strconv"
+
+	"github.com/g3n/engine/gui"
+)
+
 type Performance struct {
 	Panel *gui.Panel
 	Value, Units *gui.Label
 }
 
-func (w *Performance) NewPerformance(w [2]*gui.Label, large int, label string) {
+func NewPerformance(large int, label string) (w *Performance) {
+	w = new(Performance)
+	w.Init(large, label)
+	return
+}
+
+func (w *Performance) Init(large int, label string) {
 	w.Panel = gui.NewPanel(0, 0)
 	w.Panel.SetLayout(gui.NewDockLayout())
 	w.Units = gui.NewLabel(label)
