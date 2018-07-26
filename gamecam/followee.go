9bfdbed682d185fef56d02c61dfdb2a713b412ea
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-07-26 13:22:02 +0000

    Changed to simpler Facing method

--- gamecam/followee.go
+++ gamecam/followee.go
@@ -3,7 +3,7 @@ package gamecam
 import "github.com/g3n/engine/math32"
 
 type Followee interface {
-	FacingRadiansCcwOfEast() float64
+	Facing() (float64, float64)
 	HeightToEye() float64
 	Position() math32.Vector3
 }
