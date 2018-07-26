c4cdf890d877af93cd9b9ae0b92b86d291ec9883
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-07-26 05:25:39 +0000

    Create cammode.go

---
+++ gamecam/cammode.go
@@ -0,0 +1,32 @@
+package gamecam
+
+import "github.com/amyadzuki/amystuff/bitfield"
+
+type CamMode bitfield.Uint8
+
+var ImplScreen func(CamMode) bool = nil
+
+func (cm CamMode) Screen() bool {
+	if ImplScreen != nil {
+		return ImplScreen(cm)
+	} else {
+		return cm.Any(ScreenReasons) && !cm.Any(WorldOverrides)
+	}
+}
+
+func (cm CamMode) World() bool {
+	return !cm.Screen()
+}
+
+const (
+	DefaultToScreen uint8 = iota << 1
+	ScreenButtonHeld
+	ScreenToggleOn
+	MiddleMouseHeld
+	FirstUnusedBit
+)
+
+var (
+	ScreenReasons  = DefaultToScreen | ScreenButtonHeld | ScreenToggleOn
+	WorldOverrides = MiddleMouseHeld
+)
