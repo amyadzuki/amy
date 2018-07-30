6eae72463cafddb6f76f7362d514eb43bf62c693
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-07-30 05:00:39 +0000

    Update followee.go

--- gamecam/followee.go
+++ gamecam/followee.go
@@ -3,7 +3,10 @@ package gamecam
 import "github.com/g3n/engine/math32"
 
 type Followee interface {
+	Base() float64
 	FacingNormalized() (float64, float64)
+	FrontOfEye() float64
+	HeightToCap() float64
 	HeightToEye() float64
 	Position() math32.Vector3
 }
