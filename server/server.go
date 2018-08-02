77a958ef274b9da0e230873d622b3284cf93fb4c
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-08-02 10:46:56 +0000

    Fix: new onfail API

--- server/server.go
+++ server/server.go
@@ -6,5 +6,5 @@ type Server interface {
 	Api(Backend, uint32, ...interface{})
 	Http(Backend, ...interface{})
 	Https(Backend, ...interface{})
-	Serve(Backend, string, string, string, string, onfail.OnFail)
+	Serve(Backend, string, string, string, string, ...onfail.OnFail)
 }
